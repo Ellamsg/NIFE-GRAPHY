ef3fe610fea7543ff620548792b9d6d4995819ae
ellamsg <ellamsgeorge7@gmail.com>
2022-09-05 13:44:35 -0700

    animation

--- public/index.js
+++ public/index.js
@@ -1,4 +1,3 @@
-
 //hamburger toggle
 
 $(document).ready(function () {
@@ -11,3 +10,24 @@ $(document).ready(function () {
     });
   });
 });
+
+
+//scroll on reveal animation
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
+
+  for (var i = 0; i < reveals.length; i++) {
+
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
