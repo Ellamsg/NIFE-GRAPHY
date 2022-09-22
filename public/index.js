a3e8174c14929cc5264d76c38b8ed08da6711db9
ellamsg <ellamsgeorge7@gmail.com>
2022-09-22 12:53:14 -0700

    page animation

--- public/index.js
+++ public/index.js
@@ -1,7 +1,10 @@
+
 gsap.from('.header',{duration:1, y:'-100%',ease:'bounce'})
 gsap.from('.link',{duration:2,opacity:0, stagger:.4})
 gsap.from('.right',{duration:1,delay:.5,x:'100%'})
 gsap.from('.left',{duration:1,delay:.5,x:'-100%'})
+
+
 //hamburger toggle
 
 $(document).ready(function () {
@@ -14,51 +17,6 @@ $(document).ready(function () {
     });
   });
 });
-
-
-/*
-window.addEventListener('scroll', reveal);
-
-function reveal() {
-  var reveals = document.querySelectorAll('.reveal');
-
-  for (var i = 0; i < reveals.length; i++) {
-
-    var windowheight = window.innerHeight;
-    var revealtop = reveals[i].getBoundingClientRect().top;
-    var revealpoint = 150;
-
-    if (revealtop < windowheight - revealpoint) {
-      reveals[i].classList.add('active');
-    } else {
-      reveals[i].classList.remove('active');
-    }
-  }
-}
-*/
-
-const callback = function (entries) {
-  entries.forEach((entry) => {
-    console.log(entry);
-
-    if (entry.isIntersecting) {
-      entry.target.classList.add("animate-fadeIn");
-    } else {
-      entry.target.classList.remove("animate-fadeIn");
-    }
-  });
-};
-
-const observer = new IntersectionObserver(callback);
-
-const targets = document.querySelectorAll(".js-show-on-scroll");
-targets.forEach(function (target) {
-  target.classList.add("opacity-0");
-  observer.observe(target);
-});
-
-
-
 
 
 
@@ -147,3 +105,7 @@ TweenMax.to('.swiper-slide', 0, {
 TweenMax.to('.swiper-slide-active', 0, {
   scale: 1,
 })
+
+
+
+
