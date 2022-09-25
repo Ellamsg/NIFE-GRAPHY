d910330416b757b95907d07a8ec9d18dccf87912
ellamsg <ellamsgeorge7@gmail.com>
2022-09-25 12:27:00 -0700

    new

--- public/animate.js
+++ public/animate.js
@@ -1,4 +1,4 @@
-  
+//page transition
 const link2 = document.querySelector(".link2");
 const transition = document.querySelector(".transition");
 link2.addEventListener("click", (e) => {
@@ -20,7 +20,6 @@ const callback = function (entries) {
     }
   });
 };
-
 const observer = new IntersectionObserver(callback);
 
 const targets = document.querySelectorAll(".js-show-on-scroll");
@@ -29,24 +28,31 @@ targets.forEach(function (target) {
   observer.observe(target);
 });
 
-
 //numbers onclick background changer
-let bgchange = document.getElementById("bg-change")
-let num1 = document.getElementById("click1")
-let num2 = document.getElementById("click2")
-let  num3 = document.getElementById("click3")
-let  num4 = document.getElementById("click4")
+let bgchange = document.getElementById("bg-change");
+let num1 = document.getElementById("click1");
+let num2 = document.getElementById("click2");
+let num3 = document.getElementById("click3");
+let num4 = document.getElementById("click4");
+let num5 = document.getElementById("click5");
+num1.addEventListener("click", () => {
+  bgchange.style.backgroundImage = "url(page/eye.jpg)";
+  bgchange.style.transition = "all 2s";
+});
+num2.addEventListener("click", () => {
+  bgchange.style.backgroundImage = "url(page/girlsad.jfif)";
+  bgchange.style.transition = "all 2s";
+});
+num3.addEventListener("click", () => {
+  bgchange.style.backgroundImage = "url(page/pink.jpg)";
+  bgchange.style.transition = "all 2s";
+});
+num4.addEventListener("click", () => {
+  bgchange.style.backgroundImage = "url(page/manknown.jpg)";
+  bgchange.style.transition = "all 2s";
+});
+num5.addEventListener("click", () => {
+  bgchange.style.backgroundImage = "url(page/girlonafro.jpeg)";
+  bgchange.style.transition = "all 2s";
+});
 
-num1.addEventListener("click",()=>{
-  bgchange.style.backgroundImage = "url(page/bandit.jpg)"
-  
-})
-num2.addEventListener("click",()=>{
-  bgchange.style.backgroundImage = "url(page/girlsad.jfif)"
-})
-num3.addEventListener("click",()=>{
-  bgchange.style.backgroundImage = "url(page/pink.jpg)"
-})
-num4.addEventListener("click",()=>{
-  bgchange.style.backgroundImage = "url(page/bandit.jpg)"
-})
--- public/index.js
+++ public/index.js
@@ -1,13 +1,7 @@
-
-
-
-gsap.from('.header',{duration:1, y:'-100%',ease:'bounce'})
-gsap.from('.link',{duration:2,opacity:0, stagger:.4})
-gsap.from('.right',{duration:1,delay:.5,x:'100%'})
-gsap.from('.left',{duration:1,delay:.5,x:'-100%'})
-
-
-
+gsap.from(".header", { duration: 1, y: "-100%", ease: "bounce" });
+gsap.from(".link", { duration: 2, opacity: 0, stagger: 0.4 });
+gsap.from(".right", { duration: 1, delay: 0.5, x: "100%" });
+gsap.from(".left", { duration: 1, delay: 0.5, x: "-100%" });
 
 //hamburger toggle
 
@@ -22,97 +16,87 @@ $(document).ready(function () {
   });
 });
 
-
-
-
-
-
 var slider = new Swiper(".swiper-container", {
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   spaceBetween: 150,
   centeredSlides: true,
-  mousewheel: true
-})
+  mousewheel: true,
+});
 
-slider.on('slideChange', function () {
-  TweenMax.to('.slide-text span', .2, {
-    y: '-100px',
-  })
-  TweenMax.to('.slide-number span', .2, {
-    x: '-100px',
-  })
-  TweenMax.to('.swiper-slide-active', .5, {
-    scale: .85
-  })
-})
+slider.on("slideChange", function () {
+  TweenMax.to(".slide-text span", 0.2, {
+    y: "-100px",
+  });
+  TweenMax.to(".slide-number span", 0.2, {
+    x: "-100px",
+  });
+  TweenMax.to(".swiper-slide-active", 0.5, {
+    scale: 0.85,
+  });
+});
 
-slider.on('slideChangeTransitionEnd', function () {
-
-  TweenMax.to('.slide-text span', .2, {
+slider.on("slideChangeTransitionEnd", function () {
+  TweenMax.to(".slide-text span", 0.2, {
     y: 0,
-    delay: .5
-  })
-  TweenMax.to('.slide-text span', 0, {
-    y: '100px',
-  })
+    delay: 0.5,
+  });
+  TweenMax.to(".slide-text span", 0, {
+    y: "100px",
+  });
 
-  TweenMax.to('.slide-number span', .2, {
+  TweenMax.to(".slide-number span", 0.2, {
     x: 0,
-    delay: .7
-  })
-  TweenMax.to('.slide-number span', 0, {
-    x: '100px',
-  })
+    delay: 0.7,
+  });
+  TweenMax.to(".slide-number span", 0, {
+    x: "100px",
+  });
 
-  TweenMax.to('.swiper-slide-active', .5, {
+  TweenMax.to(".swiper-slide-active", 0.5, {
     scale: 1,
     ease: Power4.easeOut,
-  })
+  });
 
-  TweenMax.to('.swiper-slide-active .slide-text', 0, {
-    autoAlpha: 1
-  })
-  TweenMax.to('.swiper-slide-active .slide-number', 0, {
-    autoAlpha: 1
-  })
+  TweenMax.to(".swiper-slide-active .slide-text", 0, {
+    autoAlpha: 1,
+  });
+  TweenMax.to(".swiper-slide-active .slide-number", 0, {
+    autoAlpha: 1,
+  });
 
-  TweenMax.to('.swiper-slide-next .slide-text', 0, {
-    autoAlpha: 0
-  })
-  TweenMax.to('.swiper-slide-prev .slide-text', 0, {
-    autoAlpha: 0
-  })
+  TweenMax.to(".swiper-slide-next .slide-text", 0, {
+    autoAlpha: 0,
+  });
+  TweenMax.to(".swiper-slide-prev .slide-text", 0, {
+    autoAlpha: 0,
+  });
 
-  TweenMax.to('.swiper-slide-next .slide-number', 0, {
-    autoAlpha: 0
-  })
-  TweenMax.to('.swiper-slide-prev .slide-number', 0, {
-    autoAlpha: 0
-  })
-})
+  TweenMax.to(".swiper-slide-next .slide-number", 0, {
+    autoAlpha: 0,
+  });
+  TweenMax.to(".swiper-slide-prev .slide-number", 0, {
+    autoAlpha: 0,
+  });
+});
 
-TweenMax.to('.swiper-slide-next .slide-text', 0, {
-  autoAlpha: 0
-})
-TweenMax.to('.swiper-slide-prev .slide-text', 0, {
-  autoAlpha: 0
-})
+TweenMax.to(".swiper-slide-next .slide-text", 0, {
+  autoAlpha: 0,
+});
+TweenMax.to(".swiper-slide-prev .slide-text", 0, {
+  autoAlpha: 0,
+});
 
-TweenMax.to('.swiper-slide-next .slide-number', 0, {
-  autoAlpha: 0
-})
-TweenMax.to('.swiper-slide-prev .slide-number', 0, {
-  autoAlpha: 0
-})
+TweenMax.to(".swiper-slide-next .slide-number", 0, {
+  autoAlpha: 0,
+});
+TweenMax.to(".swiper-slide-prev .slide-number", 0, {
+  autoAlpha: 0,
+});
 
-TweenMax.to('.swiper-slide', 0, {
-  scale: .85,
-})
+TweenMax.to(".swiper-slide", 0, {
+  scale: 0.85,
+});
 
-TweenMax.to('.swiper-slide-active', 0, {
+TweenMax.to(".swiper-slide-active", 0, {
   scale: 1,
-})
-
-
-
-
+});
